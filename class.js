ee2763c968ca8aec878376ec6a4d90fe926df10f
Sebastião Félix <sebamorais0@gmail.com>
2023-03-28 20:54:37 -0300

    Add files via upload

---
+++ class.js
@@ -0,0 +1,30 @@
+export class Pages {
+    routes = {}
+
+    add(name, page){
+        this.routes[name] = page
+    }    
+
+    route(event){
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle()
+    }
+
+    handle(){
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+        fetch(route)
+        .then(data => data.text())
+        .then(html => {
+            document.querySelector('#page').innerHTML = html
+        })
+    }
+
+   
+
+}
+
---
+++ main.js
@@ -0,0 +1,12 @@
+import { Pages } from './class.js'
+
+const pages = new Pages()
+pages.add("/", "/pages/home.html")
+pages.add('/oUniverso', '/pages/universo.html')
+pages.add('/exploracao', '/pages/exploracao.html')
+pages.add(404, '/pages/404.html')
+
+pages.handle()
+
+window.onpopstate = () => pages.handle()
+window.route = () => pages.route()
